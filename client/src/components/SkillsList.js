a9285c451fe53cfb6a6be6f8cb39d88465f45cbf
Nicholas <najernigin@gmail.com>
2024-08-17 21:20:44 -0500

    Skills Section Revamp

--- client/src/components/SkillsList.js
+++ client/src/components/SkillsList.js
@@ -24,52 +24,45 @@ function SkillsList() {
 
   return (
     <div className={isScrolled ? "skills-container" : ""}>
-      <div id="skills-image-container">
+      <div id="skills-container">
         <div id="skills-header-container">
           <h1 id="skills-header">Skills</h1>
-          <h1 id="skills-description">
+          {/* <h1 id="skills-description">
             {"  "}
             to pay the bills <FontAwesomeIcon icon={faCheckCircle} />
-          </h1>
+          </h1> */}
         </div>
 
-        <img
-          id="skills-image"
-          src={skillsImg}
-          width="300"
-          height="200"
-          alt="skills-img"
-        />
-      </div>
-      <div id="skills-lists-container">
-        <div>
-          <h1>
-            Hard Skills <FontAwesomeIcon color="dodgerBlue" icon={faTools} />
-          </h1>
-          <ul>
-            <li>Git</li>
-            <li>Javascript</li>
-            <li>Python</li>
-            <li>SQL</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>React</li>
-            <li>Flask</li>
-            <li>Bootstrap</li>
-          </ul>
-        </div>
-        <div>
-          <h1>
-            Soft Skills{" "}
-            <FontAwesomeIcon color="dodgerBlue" icon={faHandshake} />
-          </h1>
-          <ul>
-            <li>Interpersonal Skills</li>
-            <li>Collaborative</li>
-            <li>Analytical</li>
-            <li>Life Long Learner</li>
-            <li>Tactful</li>
-          </ul>
+        <div id="skills-lists-container">
+          <div>
+            <h1>
+              Hard Skills <FontAwesomeIcon color="dodgerBlue" icon={faTools} />
+            </h1>
+            <ul>
+              <li>Git</li>
+              <li>Javascript</li>
+              <li>Python</li>
+              <li>SQL</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>React</li>
+              <li>Flask</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <div>
+            <h1>
+              Soft Skills{" "}
+              <FontAwesomeIcon color="dodgerBlue" icon={faHandshake} />
+            </h1>
+            <ul>
+              <li>Interpersonal Skills</li>
+              <li>Collaborative</li>
+              <li>Analytical</li>
+              <li>Life Long Learner</li>
+              <li>Tactful</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
